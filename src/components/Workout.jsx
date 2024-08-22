56c5c83af03d3210f4518c26758560d525f3a09e
Dawood Faisal <dawood_faisal@yahoo.com>
2024-08-23 03:33:04 +0500

    commit-15

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -7,7 +7,7 @@ export default function Workout(props) {
   const { workout } = props
 
   return (
-    <SectionWrapper id={'workout'} header={'Welcome to'} title={['The', 'DANGER', ' zone']}> 
+    <SectionWrapper id={'workout'} header={'Welcome to'} title={['The', 'DANGER', ' Zone']}> 
       <div className='flex flex-col gap-4'>
         {workout.map((excercise, i) =>{
           return(
